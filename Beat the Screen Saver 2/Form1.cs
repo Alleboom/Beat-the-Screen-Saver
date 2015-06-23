da4fd25d5969b0aad9fc93d0e4beb2f6f023bed7
Alleboom <zachmosch1990@gmail.com>
2015-06-22 23:25:05 -0400

    Presses the ctrl key every second while active
    A small multi-threaded program that press ctrl once every second.
    Basically the idea was to have systems that were not on the network to
    have the ability to 'disable' their screen saver, as a group policy will
    revert it back to having a screen saver every 15 minutes, requiring a
    log in with the original user of the system, even if they weren't
    available.

---
+++ Beat the Screen Saver 2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beat_the_Screen_Saver_2
+{
+    public partial class Form1 : Form
+    {
+        bool keeprunning = false;
+        public System.Threading.Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+
+            thread = new System.Threading.Thread(sendkey);
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+
+            this.FormClosed += new FormClosedEventHandler(KillThread);
+        }
+
+        private void KillThread(object sender, EventArgs e)
+        {
+            keeprunning = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            keeprunning = true;
+            pictureBox1.Visible = true; 
+            pictureBox2.Visible = false;
+            thread = new System.Threading.Thread(sendkey);
+            thread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            keeprunning = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+        }
+
+        public void sendkey(object o)
+        {
+            while (keeprunning)
+            {
+                System.Threading.Thread.Sleep(1000);
+                SendKeys.SendWait("^");
+            }
+        }
+
+    }
+}
